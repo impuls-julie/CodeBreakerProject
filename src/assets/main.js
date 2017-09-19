14a62dc2c6076815b18ffa61bcf09612540f6054
julie.vandebosch <julie.vandebosch@impulscommunicatie.be>
2017-09-19 14:55:24 +0200

    till validateInput

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,7 +5,7 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
 
-    add functionality to guess function here
+    // add functionality to guess function here
     if(answer === '' || attempt === '') {
       setHiddenFields();
     }
