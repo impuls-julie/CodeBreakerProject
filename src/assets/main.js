59ea26c17a19ab2a09f89cdc9a4b3c5843869d22
julie.vandebosch <julie.vandebosch@impulscommunicatie.be>
2017-09-19 14:26:39 +0200

    setHiddenFields

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,9 +1,23 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
+let message = document.getElementById('message');
 
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if(answer === '' || attempt === '') {
+      setHiddenFields();
+    }
 }
 
 //implement new functions here
+
+function setHiddenFields() {
+  answer = Math.floor(Math.random());
+  attempt = 0;
+
+  while(answer.length < 4) {
+    answer.toString();
+    answer += '0';
+  }
+}
